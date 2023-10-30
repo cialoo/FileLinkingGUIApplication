21ad63f7640bec78bb9936a89397c02a918b483c
Kacper Śniadek <k.cs.kesz@gmail.com>
2023-10-31 00:02:09 +0100

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main extends JFrame{
     private JLabel jLabel1;
     private JPanel MainPanel;
+    private JLabel jLabel2;
+    private JButton jButton1;
+    private JTextField jText1;
 
     public Main () {
         setContentPane(MainPanel);
@@ -11,6 +16,12 @@ public class Main extends JFrame{
         setSize(600, 400);
         setLocationRelativeTo(null);
         setVisible(true);
+        jButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     public static void main(String[] args) {
